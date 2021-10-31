a3678ef2e093030efbd3960b9661617bf0d4de01
Temiogundejim <yusuffogundeji@gmail.com>
2021-10-31 05:22:07 -0700

    update route

--- routes/classes.routes.js
+++ routes/classes.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.post('/class', ClassController.createClass);
 router.get('/class', ClassController.getAllClasses);
 router.get("/program/class/:programId", ClassController.getClassByProgramId);
-router.get("level/class/:levelId", ClassController.getClassesByLevelId);
+router.get("/level/class/:levelId", ClassController.getClassesByLevelId);
 router.get("/week/class/:weekId", ClassController.getClassesByWeekId);
 router.get("/course/class/:weekId", ClassController.getClassesByCourseId);
 router.get('/class/:id', ClassController.getClassById);
